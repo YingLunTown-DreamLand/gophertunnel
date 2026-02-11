f80bb9913cb758ce124993849ef77aa081f389d1
Happy2018new <chenbo79800@163.com>
2026-02-11 20:56:04 +0800

    gophertunnel: Updated for 1.26.0

--- minecraft/protocol/packet/graphics_override_parameter.go
+++ minecraft/protocol/packet/graphics_override_parameter.go
@@ -1,11 +1,18 @@
 package packet
 
-import "github.com/YingLunTown-DreamLand/gophertunnel/minecraft/protocol"
+import (
+	"github.com/YingLunTown-DreamLand/gophertunnel/minecraft/protocol"
+	"github.com/go-gl/mathgl/mgl32"
+)
 
 // GraphicsOverrideParameter is sent by the server to override graphics parameters.
 type GraphicsOverrideParameter struct {
 	// Values is a list of parameter keyframe values.
 	Values []protocol.ParameterKeyframeValue
+	// FloatValue ...
+	FloatValue float32
+	// Vec3Value ...
+	Vec3Value mgl32.Vec3
 	// BiomeIdentifier is the identifier of the biome for which the parameters apply.
 	BiomeIdentifier string
 	// ParameterType is the type of parameter being overridden.
@@ -21,6 +28,8 @@ func (*GraphicsOverrideParameter) ID() uint32 {
 
 func (pk *GraphicsOverrideParameter) Marshal(io protocol.IO) {
 	protocol.Slice(io, &pk.Values)
+	io.Float32(&pk.FloatValue)
+	io.Vec3(&pk.Vec3Value)
 	io.String(&pk.BiomeIdentifier)
 	io.Uint8(&pk.ParameterType)
 	io.Bool(&pk.Reset)
